7b5b10b24b6028eeb0a29effcd32329bdbdf0998
bugra <bugrahankaramollaoglu@gmail.com>
2024-03-17 22:58:49 +0300

    ok. will try to fetch data from frestore

--- lib/pages/sub_pages/home_screen.dart
+++ lib/pages/sub_pages/home_screen.dart
@@ -5,6 +5,8 @@ class Home_Screen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
   }
 }
